9a4718f6ac25834646414341b5e9d22b1df23e46
James <drawnkid@gmail.com>
2016-07-06 18:37:52 +0800

    update

--- src/main/java/lol/Application.java
+++ src/main/java/lol/Application.java
@@ -1,8 +1,6 @@
 package lol;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
