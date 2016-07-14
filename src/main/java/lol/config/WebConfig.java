40e95b07cadaa7d072e4d0131e043f0c1959c051
James <drawnkid@gmail.com>
2016-07-14 11:33:05 +0800

    update to using Spring Security

--- src/main/java/lol/config/WebConfig.java
+++ src/main/java/lol/config/WebConfig.java
@@ -3,6 +3,7 @@ package lol.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
 
@@ -16,6 +17,13 @@ import org.springframework.web.servlet.resource.VersionResourceResolver;
 @Configuration
 @EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter{
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/form").setViewName("form");
+
+	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
