40561f65af3494f05137b1a384c3c5527554219e
james <drawnkid@gmail.com>
2016-09-29 00:11:02 +0800

    update

--- src/main/resources/assets/js/custom/controllers/weatherController.js
+++ src/main/resources/assets/js/custom/controllers/weatherController.js
@@ -1,6 +1,6 @@
 app.controller('weatherController', function($scope, $http) {
 	$scope.weather ;
-
+	$scope.weatherInfo ;
 	$scope.getWeather = function(){
 		$http.get("/weather/query.json?cityname=北京")
 	    .success(function(response) {
@@ -10,8 +10,26 @@ app.controller('weatherController', function($scope, $http) {
 		    	var realTimeDate = result.realtime.date;
 		    	var realTimetime = result.realtime.time;
 		    	$scope.realTime = realTimeDate +" "+realTimetime;
+		    	var weekday=new Array(7);
+		    	weekday[0]="Sunday";
+		    	weekday[1]="Monday";
+		    	weekday[2]="Tuesday";
+		    	weekday[3]="Wednesday";
+		    	weekday[4]="Thursday";
+		    	weekday[5]="Firday";
+		    	weekday[6]="Saturday";
+		    	$scope.currentWeek = weekday[(result.realtime.week+1)];
+		    	$scope.currentDateTime = $scope.realTime;
+		    	$scope.cityName = result.realtime['city_name'];
+		    	$scope.weatherInfo = result.realtime.weather.info;
+		    	$scope.temperature = result.realtime.weather.temperature;
+		    	$scope.humidity = result.realtime.weather.humidity;
+
+		    	if($scope.weatherInfo.indexOf("多云")!=-1){
+		    		$scope.weatherInfoIcon = "partly-cloudy-day";
+		    	}
 	    	}else{
-	    		alert("获取天气信息失败")
+	    		 console.log("获取天气信息失败");
 	    	}
 	    });
 	}
