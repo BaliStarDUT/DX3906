9325f1d1ce2261fe0e097a973c815cd53690fbd3
y0604 <yangz0604@dtdream>
2016-10-08 18:35:51 +0800

    update spring Security

--- src/main/java/lol/config/WebSecurityConfig.java
+++ src/main/java/lol/config/WebSecurityConfig.java
@@ -2,6 +2,10 @@ package lol.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  *
@@ -10,36 +14,29 @@ import org.springframework.context.annotation.Configuration;
  * @version 1.0
  * @since
  */
-//@Configuration
-//@EnableWebSecurity
-public class WebSecurityConfig {//extends WebSecurityConfigurerAdapter {
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//    	http
-//		.authorizeRequests()
-//			.anyRequest().authenticated()
-//			.and()
-//		.formLogin()//.loginPage("/greeting")
-//			.and()
-//		.httpBasic();
-//        http
-//            .authorizeRequests()
-//	            .antMatchers("/resources/**", "/signup", "/about").permitAll()  
-//				.antMatchers("/admin/**").hasRole("ADMIN")       
-//				.antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')") 
-//				.anyRequest().authenticated() 
-//			.and()
-//		.formLogin()
-//			.permitAll(); 
-//    }
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+	            .antMatchers("/resources/**", "/signup", "/about").permitAll()  
+				.antMatchers("/admin/**").hasRole("ADMIN")       
+				.antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')") 
+				.anyRequest().authenticated() 
+			.and()
+		.formLogin().loginPage("/resources/view/login.html")
+			.permitAll(); 
+    }
 
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//            .inMemoryAuthentication()
-//                .withUser("user").password("password").roles("USER").and()
-//                .withUser("user1").password("password").roles("ADMIN").and()
-//                .withUser("user2").password("password").roles("DBA");
-//    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER").and()
+                .withUser("user1").password("password").roles("ADMIN").and()
+                .withUser("user2").password("password").roles("DBA");
+    }
     
 }
