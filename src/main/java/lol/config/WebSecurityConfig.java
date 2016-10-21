c8e4a22805b6084bcd96014f1999b465f0b69fd1
y0604 <yangz0604@dtdream>
2016-10-21 19:27:08 +0800

    update

--- src/main/java/lol/config/WebSecurityConfig.java
+++ src/main/java/lol/config/WebSecurityConfig.java
@@ -21,12 +21,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-	            .antMatchers("/resources/**", "/signup", "/about").permitAll()  
+	            .antMatchers("/resources/**","/weather/**","/signup", "/about").permitAll()  
 				.antMatchers("/admin/**").hasRole("ADMIN")       
 				.antMatchers("/db/**").access("hasRole('ADMIN') and hasRole('DBA')") 
 				.anyRequest().authenticated() 
 			.and()
-		.formLogin().loginPage("/resources/view/login.html")
+		.formLogin() //.loginPage("/resources/view/login.html")
 			.permitAll();
         http.csrf().disable();
         http.logout()
