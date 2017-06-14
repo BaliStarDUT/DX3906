185186c7c8a9fa98c2df3ac9dc846c274ee86036
James Yang <drawnkid@gmail.com>
2017-06-14 20:17:37 +0800

    修改打包的资源

--- src/main/java/top/hunaner/lol/controller/error/GlobalExceptionHandler.java
+++ src/main/java/top/hunaner/lol/controller/error/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    public static final String DEFAULT_ERROR_VIEW = "404.html";
+    public static final String DEFAULT_ERROR_VIEW = "404";
     @ExceptionHandler(value = Exception.class)
     public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
         ModelAndView mav = new ModelAndView();
